9a684d26238fd82d9afbe20dcd199e96ea82e9cc
Hércules  Ferreira <123484918+haf47@users.noreply.github.com>
2023-11-15 12:01:03 -0300

    Add files via upload

---
+++ 1-meuPrimeiroPrograma.js
@@ -0,0 +1,12 @@
+console.log ("Meu primeiro programa:trabalhando com variáveis");
+
+const idade = 62;
+
+console.log (idade);
+
+console.log (idade+12);
+
+
+const idadeSomada = idade + 20;
+
+console.log(idadeSomada);
